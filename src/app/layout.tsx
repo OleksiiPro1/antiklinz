d349ec0131360e74a61b4b4b85e248ac97eeabcb
OleksiiPro1 <ukr.top@ukr.net>
2025-09-18 15:47:39 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
    icons: {
-    icon: '/apple-touch-icon.webp',
-    shortcut: '/apple-touch-icon.webp',
-    apple: '/apple-touch-icon.webp'
+    icon: 'favicon.ico',
+    shortcut: 'favicon.ico',
+    apple: 'favicon.ico'
   },
   title: 'Antiquitäten Ankauf',
   description: 'Antiquitäten Ankauf. Lesen Sie unsere Tipps, Ratgeber und Neuigkeiten rund um Antiquitäten und Möbel.',
@@ -18,19 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="de">
       <head>
         <meta charSet="utf-8" />
-        
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4C6TMSBV48"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-4C6TMSBV48');
-            `,
-          }}
-        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className="font-sans text-dark">
         <Header />
